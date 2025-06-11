7c2199f8d3807ca900c487fa3b17b73c57bfe853
LucasTNM <148718831+LucasTNM@users.noreply.github.com>
2025-06-11 19:05:10 -0300

    Fix em operações

--- App.py
+++ App.py
@@ -1,9 +1,4 @@
-# Criar um sistema Bancario com as operações de saque, depositar e vizualizar extrato
-# Deposito: somente valores > 0  e todos os depositos devem ser armazenados em uma variável
-# Saque: permitido até 3 saques no valor máximo de 500 reais, verificar se o cliente tem saldo em conta para sacar -m"Saldo insuficiente". Armazenar todos os saques
-# Extrato: deve listar todos os depósitos e saques realizados e no fim exibir o saldo atual da conta no formato R$ xxx.xx
-
-menu = """"
+menu = """
 
 [d] Depositar
 [s] Sacar
@@ -23,7 +18,7 @@ while True:
     opcao = input(menu)
 
     if opcao == "d":
-        print("Depósito")
+        print("\nDepósito")
         deposito = float(input("Digite o valor do depósito: "))
         
         if deposito < 0: 
@@ -34,21 +29,27 @@ while True:
         extrato += f"Depósito: R$ {deposito:.2f}\n"
     
     elif opcao == "s":
-        print("Saque")
-        print(f"Saldo da conta {saldo}")
+        print("\nSaque")
+        
         if saldo > 0 and numero_saques <= LIMITE_SAQUES:
             valor_saque = float(input("Valor do Saque: "))
+            
             if valor_saque > limite:
                 print(f"Saque tem o limite de R$ {limite}")
                 continue
             saldo -= valor_saque
             extrato += f"Saque: R$ {valor_saque:.2f}\n"
+        
         else:
             print("Saldo insuficiente.")
             continue
     
     elif opcao == "e":
-        print("Extrato")
+        if not extrato:
+            print("\nSem movimentações na conta.")
+            continue
+        
+        print("\nExtrato")
         print(extrato)
         print(f"Saldo: R$ {saldo:.2f}")
         
